fffdba4721430b3e1eafb461662c004b0ced0197
miyamo2 <miyamo2@outlook.com>
2024-03-17 22:31:22 +0900

    added mocks

---
+++ mockgen.go
@@ -0,0 +1,3 @@
+//go:generate mockgen -destination=internal/mocks/mock_gorm_expression.go -package=mocks gorm.io/gorm/clause Expression
+//go:generate mockgen -destination=internal/mocks/mock_gorm_builder.go -package=mocks gorm.io/gorm/clause Builder
+package dynamgorm
