d2543a0a91b3e7aad3634c338de76a88c1a8a72f
miyamo2 <miyamo2@outlook.com>
2024-03-20 10:02:53 +0900

    secondaryIndexExpression impl gorm.StatementModifier etc

--- secondary_index.go
+++ secondary_index.go
@@ -9,17 +9,69 @@ import (
 
 // compatibility
 var _ clause.Expression = (*secondaryIndexExpression)(nil)
+var _ gorm.StatementModifier = (*secondaryIndexExpression)(nil)
+
+type SecondaryIndexOption func(*secondaryIndexExpression)
 
 // SecondaryIndexOf is the table with the index to be used.
-type SecondaryIndexOf interface {
-	string | clause.Table
+func SecondaryIndexOf[T string | clause.Table](table T) SecondaryIndexOption {
+	return func(s *secondaryIndexExpression) {
+		switch table := (interface{})(table).(type) {
+		case string:
+			s.tableName = table
+		case clause.Table:
+			s.table = table
+		}
+	}
+}
+
+// ModelAs is the model to be used for the index.
+func ModelAs(dest interface{}) SecondaryIndexOption {
+	return func(s *secondaryIndexExpression) {
+		s.model = dest
+	}
 }
 
 // secondaryIndexExpression is a clause.Expression that represents a secondary index
 type secondaryIndexExpression struct {
 	table     clause.Table
+	model     interface{}
 	tableName string
 	indexName string
+}
+
+func (s secondaryIndexExpression) ModifyStatement(stmt *gorm.Statement) {
+	m := s.model
+	if m != nil {
+		stmt.Dest = m
+	}
+
+	tn := s.tableName
+	if tn == "" {
+		tn = s.table.Name
+	}
+	if tn == "" {
+		// if specified indexName is in the format "table_name.index_name", then split it into table_name and index_name
+		// e.g.
+		//	dynmgrm.SecondaryIndex("table_name.index_name")
+		stn := strings.Split(s.indexName, ".")
+		if len(stn) == 2 {
+			tn = stn[0]
+			s.indexName = stn[1]
+		}
+	}
+	if tn == "" {
+		tn = stmt.Table
+	}
+
+	stmt.Table = fmt.Sprintf(`%s.%s`, tn, s.indexName)
+	qtn := &strings.Builder{}
+	stmt.Dialector.QuoteTo(qtn, tn)
+	qin := &strings.Builder{}
+	stmt.Dialector.QuoteTo(qin, s.indexName)
+	stmt.TableExpr = &clause.Expr{
+		SQL: fmt.Sprintf(`%s.%s`, qtn.String(), qin.String()),
+	}
 }
 
 // Build builds the secondaryIndexExpression
@@ -28,38 +80,15 @@ func (s secondaryIndexExpression) Build(builder clause.Builder) {
 	if !ok {
 		return
 	}
-	tn := s.tableName
-	if tn == "" {
-		tn = s.table.Name
-	}
-	stmt.Table = fmt.Sprintf(`%s.%s`, tn, s.indexName)
-
-	qtn := &strings.Builder{}
-	stmt.Dialector.QuoteTo(qtn, tn)
-	qin := &strings.Builder{}
-	stmt.Dialector.QuoteTo(qin, s.indexName)
-
-	stmt.TableExpr = &clause.Expr{
-		SQL:                fmt.Sprintf(`%s.%s`, qtn.String(), qin.String()),
-		Vars:               nil,
-		WithoutParentheses: false,
-	}
+	s.ModifyStatement(stmt)
 }
 
 // SecondaryIndex enables queries using a secondary index
-func SecondaryIndex[T SecondaryIndexOf](table T, indexName string) secondaryIndexExpression {
+func SecondaryIndex(indexName string, options ...SecondaryIndexOption) secondaryIndexExpression {
 	var xp secondaryIndexExpression
-	switch table := (interface{})(table).(type) {
-	case string:
-		xp = secondaryIndexExpression{
-			tableName: table,
-			indexName: indexName,
-		}
-	case clause.Table:
-		xp = secondaryIndexExpression{
-			table:     table,
-			indexName: indexName,
-		}
+	xp.indexName = indexName
+	for _, opt := range options {
+		opt(&xp)
 	}
 	return xp
 }
--- secondary_index_test.go
+++ secondary_index_test.go
@@ -13,10 +13,14 @@ func TestSecondaryIndexExpression_Build(t *testing.T) {
 		builder            clause.Builder
 		exprectedTable     string
 		exprectedTableExpr clause.Expr
+		expectedDist       interface{}
 	}
+
+	type TestTable struct{}
+
 	tests := map[string]test{
-		"happy-path/with-table-name": {
-			sut: SecondaryIndex("testTable", "testIndex"),
+		"happy-path": {
+			sut: SecondaryIndex("testTable.testIndex"),
 			builder: &gorm.Statement{
 				DB: &gorm.DB{
 					Config: &gorm.Config{
@@ -29,12 +33,39 @@ func TestSecondaryIndexExpression_Build(t *testing.T) {
 			},
 			exprectedTable: "testTable.testIndex",
 		},
-		"happy-path/with-table-clause": {
-			sut: SecondaryIndex(
-				clause.Table{
-					Name: "testTable",
-				}, "testIndex"),
+		"happy-path/with-secondary-index-of": {
+			sut: SecondaryIndex("testIndex", SecondaryIndexOf("testTable")),
 			builder: &gorm.Statement{
+				DB: &gorm.DB{
+					Config: &gorm.Config{
+						Dialector: &mockDialector{},
+					},
+				},
+			},
+			exprectedTableExpr: clause.Expr{
+				SQL: `"testTable"."testIndex"`,
+			},
+			exprectedTable: "testTable.testIndex",
+		},
+		"happy-path/with-model-as": {
+			sut: SecondaryIndex("testTable.testIndex", ModelAs(TestTable{})),
+			builder: &gorm.Statement{
+				DB: &gorm.DB{
+					Config: &gorm.Config{
+						Dialector: &mockDialector{},
+					},
+				},
+			},
+			exprectedTableExpr: clause.Expr{
+				SQL: `"testTable"."testIndex"`,
+			},
+			exprectedTable: "testTable.testIndex",
+			expectedDist:   TestTable{},
+		},
+		"happy-path/with-statement-table": {
+			sut: SecondaryIndex("testIndex"),
+			builder: &gorm.Statement{
+				Table: "testTable",
 				DB: &gorm.DB{
 					Config: &gorm.Config{
 						Dialector: &mockDialector{},
@@ -57,6 +88,10 @@ func TestSecondaryIndexExpression_Build(t *testing.T) {
 				}
 				if diff := cmp.Diff(statement.TableExpr, &tt.exprectedTableExpr); diff != "" {
 					t.Errorf("Statement.TableExpr mismatch (-want +got):\n%s", diff)
+					return
+				}
+				if diff := cmp.Diff(statement.Dest, tt.expectedDist); diff != "" {
+					t.Errorf("Statement.Dest mismatch (-want +got):\n%s", diff)
 					return
 				}
 			}
