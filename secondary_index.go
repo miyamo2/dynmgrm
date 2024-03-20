ba5ce739de1dc1d76da71c217da21a7ee0be26ab
miyamo2 <79917704+miyamo2@users.noreply.github.com>
2024-03-20 10:08:16 +0900

    [Feat] #5 #6 Add CustomClause for Secondary Index (#13)
    * added SecondaryIndex Expression
    
    * secondaryIndexExpression impl gorm.StatementModifier etc
    
    * added godoc for SecondaryIndexOption

---
+++ secondary_index.go
@@ -0,0 +1,96 @@
+package dynamgorm
+
+import (
+	"fmt"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+	"strings"
+)
+
+// compatibility
+var _ clause.Expression = (*secondaryIndexExpression)(nil)
+var _ gorm.StatementModifier = (*secondaryIndexExpression)(nil)
+
+// SecondaryIndexOption is a functional option for secondaryIndexExpression
+type SecondaryIndexOption func(*secondaryIndexExpression)
+
+// SecondaryIndexOf is the table with the index to be used.
+func SecondaryIndexOf[T string | clause.Table](table T) SecondaryIndexOption {
+	return func(s *secondaryIndexExpression) {
+		switch table := (interface{})(table).(type) {
+		case string:
+			s.tableName = table
+		case clause.Table:
+			s.table = table
+		}
+	}
+}
+
+// ModelAs is the model to be used for the index.
+func ModelAs(dest interface{}) SecondaryIndexOption {
+	return func(s *secondaryIndexExpression) {
+		s.model = dest
+	}
+}
+
+// secondaryIndexExpression is a clause.Expression that represents a secondary index
+type secondaryIndexExpression struct {
+	table     clause.Table
+	model     interface{}
+	tableName string
+	indexName string
+}
+
+// ModifyStatement modifies the gorm.Statement to use the secondary index
+func (s secondaryIndexExpression) ModifyStatement(stmt *gorm.Statement) {
+	m := s.model
+	if m != nil {
+		stmt.Dest = m
+	}
+
+	tn := s.tableName
+	if tn == "" {
+		tn = s.table.Name
+	}
+	if tn == "" {
+		// if specified indexName is in the format "table_name.index_name", then split it into table_name and index_name
+		// e.g.
+		//	dynmgrm.SecondaryIndex("table_name.index_name")
+		stn := strings.Split(s.indexName, ".")
+		if len(stn) == 2 {
+			tn = stn[0]
+			s.indexName = stn[1]
+		}
+	}
+	if tn == "" {
+		tn = stmt.Table
+	}
+
+	stmt.Table = fmt.Sprintf(`%s.%s`, tn, s.indexName)
+	qtn := &strings.Builder{}
+	stmt.Dialector.QuoteTo(qtn, tn)
+	qin := &strings.Builder{}
+	stmt.Dialector.QuoteTo(qin, s.indexName)
+	stmt.TableExpr = &clause.Expr{
+		SQL: fmt.Sprintf(`%s.%s`, qtn.String(), qin.String()),
+	}
+}
+
+// Build builds the secondaryIndexExpression
+func (s secondaryIndexExpression) Build(builder clause.Builder) {
+	stmt, ok := builder.(*gorm.Statement)
+	if !ok {
+		return
+	}
+	s.ModifyStatement(stmt)
+}
+
+// SecondaryIndex enables queries using a secondary index
+func SecondaryIndex(indexName string, options ...SecondaryIndexOption) secondaryIndexExpression {
+	var xp secondaryIndexExpression
+	xp.indexName = indexName
+	for _, opt := range options {
+		opt(&xp)
+	}
+	return xp
+}
---
+++ secondary_index_test.go
@@ -0,0 +1,100 @@
+package dynamgorm
+
+import (
+	"github.com/google/go-cmp/cmp"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+	"testing"
+)
+
+func TestSecondaryIndexExpression_Build(t *testing.T) {
+	type test struct {
+		sut                secondaryIndexExpression
+		builder            clause.Builder
+		exprectedTable     string
+		exprectedTableExpr clause.Expr
+		expectedDist       interface{}
+	}
+
+	type TestTable struct{}
+
+	tests := map[string]test{
+		"happy-path": {
+			sut: SecondaryIndex("testTable.testIndex"),
+			builder: &gorm.Statement{
+				DB: &gorm.DB{
+					Config: &gorm.Config{
+						Dialector: &mockDialector{},
+					},
+				},
+			},
+			exprectedTableExpr: clause.Expr{
+				SQL: `"testTable"."testIndex"`,
+			},
+			exprectedTable: "testTable.testIndex",
+		},
+		"happy-path/with-secondary-index-of": {
+			sut: SecondaryIndex("testIndex", SecondaryIndexOf("testTable")),
+			builder: &gorm.Statement{
+				DB: &gorm.DB{
+					Config: &gorm.Config{
+						Dialector: &mockDialector{},
+					},
+				},
+			},
+			exprectedTableExpr: clause.Expr{
+				SQL: `"testTable"."testIndex"`,
+			},
+			exprectedTable: "testTable.testIndex",
+		},
+		"happy-path/with-model-as": {
+			sut: SecondaryIndex("testTable.testIndex", ModelAs(TestTable{})),
+			builder: &gorm.Statement{
+				DB: &gorm.DB{
+					Config: &gorm.Config{
+						Dialector: &mockDialector{},
+					},
+				},
+			},
+			exprectedTableExpr: clause.Expr{
+				SQL: `"testTable"."testIndex"`,
+			},
+			exprectedTable: "testTable.testIndex",
+			expectedDist:   TestTable{},
+		},
+		"happy-path/with-statement-table": {
+			sut: SecondaryIndex("testIndex"),
+			builder: &gorm.Statement{
+				Table: "testTable",
+				DB: &gorm.DB{
+					Config: &gorm.Config{
+						Dialector: &mockDialector{},
+					},
+				},
+			},
+			exprectedTableExpr: clause.Expr{
+				SQL: `"testTable"."testIndex"`,
+			},
+			exprectedTable: "testTable.testIndex",
+		},
+	}
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			tt.sut.Build(tt.builder)
+			if statement, ok := tt.builder.(*gorm.Statement); ok {
+				if diff := cmp.Diff(statement.Table, tt.exprectedTable); diff != "" {
+					t.Errorf("Statement.Table mismatch (-want +got):\n%s", diff)
+					return
+				}
+				if diff := cmp.Diff(statement.TableExpr, &tt.exprectedTableExpr); diff != "" {
+					t.Errorf("Statement.TableExpr mismatch (-want +got):\n%s", diff)
+					return
+				}
+				if diff := cmp.Diff(statement.Dest, tt.expectedDist); diff != "" {
+					t.Errorf("Statement.Dest mismatch (-want +got):\n%s", diff)
+					return
+				}
+			}
+		})
+	}
+}
